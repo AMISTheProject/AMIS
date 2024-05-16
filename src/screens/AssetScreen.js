8598b6d00751b904feaa2c11e4f9a7fd0cd562a5
Cristian Scobioala - Nicoglu <t8220137@aueb.gr>
2024-05-16 12:14:55 +0300

    Differantiate classes of prefixed fields in AUC for 'not allowed' cursor sign

--- src/screens/AssetScreen.js
+++ src/screens/AssetScreen.js
@@ -6,7 +6,7 @@ export const AssetScreen = ({title}) => {
         <div className='asset-screen-container'>
             <div className='asset-screen-title-container'>
                 <h3 className='asset-screen-title'>{title}</h3>
-                <PrimaryButton text='Save changes' bgColor='red' color='#fff'/>
+                <PrimaryButton className="auc-save-button" text='Save changes' bgColor='red' color='#fff'/>
             </div>
 
             <div className='asset-screen-form-container'>
@@ -16,27 +16,27 @@ export const AssetScreen = ({title}) => {
                     <div className='asset-screen-fields-container'>
                         <div className='asset-screen-input-container'>
                             <label className='asset-screen-input-label' for="plant-name">Plant name</label>
-                            <input className='asset-screen-input-field' type="text" id="plant-name" name="plant-name" value='Schimatari' disabled='true'></input>
+                            <input className='asset-screen-input-field-prefixed' type="text" id="plant-name" name="plant-name" value='Schimatari' disabled='true'></input>
                         </div>
 
                         <div className='asset-screen-input-container'>
                             <label className='asset-screen-input-label' for="category">Category</label>
-                            <input className='asset-screen-input-field' type="text" id="category" name="category" value='Building' disabled='true'></input>
+                            <input className='asset-screen-input-field-prefixed' type="text" id="category" name="category" value='Building' disabled='true'></input>
                         </div>
 
                         <div className='asset-screen-input-container'>
                             <label className='asset-screen-input-label' for="wbs-element">WBS Element</label>
-                            <input className='asset-screen-input-field' type="text" id="wbs-element" name="wbs-element" value='3857565763' disabled='true'></input>
+                            <input className='asset-screen-input-field-prefixed' type="text" id="wbs-element" name="wbs-element" value='3857565763' disabled='true'></input>
                         </div>
 
                         <div className='asset-screen-input-container'>
                             <label className='asset-screen-input-label' for="wbs-desc">WBS Description</label>
-                            <input className='asset-screen-input-field' type="text" id="wbs-desc" name="wbs-desc" value='Schimatari Building B - RGB Line Project' disabled='true'></input>
+                            <input className='asset-screen-input-field-prefixed' type="text" id="wbs-desc" name="wbs-desc" value='Schimatari Building B - RGB Line Project' disabled='true'></input>
                         </div>
 
                         <div className='asset-screen-input-container'>
-                            <label className='asset-screen-input-label' for="wbs-desc">WBS Description</label>
-                            <input className='asset-screen-input-field' type="text" id="wbs-desc" name="wbs-desc" value='Schimatari Building B - RGB Line Project' disabled='true'></input>
+                            <label className='asset-screen-input-label' for="proj-eng">Project Engineer</label>
+                            <input className='asset-screen-input-field-prefixed' type="text" id="proj-eng" name="proj-eng" value='Solon Venetsanos' disabled='true'></input>
                         </div>
                     </div>
                 </div>
@@ -45,28 +45,28 @@ export const AssetScreen = ({title}) => {
 
                     <div className='asset-screen-fields-container'>
                         <div className='asset-screen-input-container'>
-                            <label className='asset-screen-input-label' for="plant-name">Plant name</label>
-                            <input className='asset-screen-input-field' type="text" id="plant-name" name="plant-name" placeholder='Schimatari'></input>
-                        </div>
-
-                        <div className='asset-screen-input-container'>
                             <label className='asset-screen-input-label' for="category">Category</label>
-                            <input className='asset-screen-input-field' type="text" id="category" name="category" ></input>
+                            <input className='asset-screen-input-field' type="text" id="category" name="category" placeholder='Main Asset / Improvement'></input>
                         </div>
 
                         <div className='asset-screen-input-container'>
-                            <label className='asset-screen-input-label' for="wbs-element">WBS Element</label>
-                            <input className='asset-screen-input-field' type="text" id="wbs-element" name="wbs-element" ></input>
+                            <label className='asset-screen-input-label' for="category">Date</label>
+                            <input className='asset-screen-input-field' type="text" id="date" name="date" ></input>
                         </div>
 
                         <div className='asset-screen-input-container'>
-                            <label className='asset-screen-input-label' for="wbs-desc">WBS Description</label>
-                            <input className='asset-screen-input-field' type="text" id="wbs-desc" name="wbs-desc" ></input>
+                            <label className='asset-screen-input-label' for="main-asset-number">Main asset number</label>
+                            <input className='asset-screen-input-field' type="text" id="main-asset-number" name="main-asset-number" ></input>
                         </div>
 
                         <div className='asset-screen-input-container'>
-                            <label className='asset-screen-input-label' for="wbs-desc">WBS Description</label>
-                            <input className='asset-screen-input-field' type="text" id="wbs-desc" name="wbs-desc" ></input>
+                            <label className='asset-screen-input-label' for="extension">Extension of useful life</label>
+                            <input className='asset-screen-input-field' type="text" id="extension" name="extension" ></input>
+                        </div>
+
+                        <div className='asset-screen-input-container'>
+                            <label className='asset-screen-input-label' for="reason">Reason for extension</label>
+                            <input className='asset-screen-input-field' type="text" id="reason" name="reason" ></input>
                         </div>
                     </div>
                 </div>
