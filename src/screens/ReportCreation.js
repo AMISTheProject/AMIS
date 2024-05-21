7e28e2510f13655221497fc8ab0e1cbcfcde9a93
crissel04 <t8220135@aueb.gr>
2024-05-22 00:00:21 +0300

    Fix ReportCreation screen

--- src/screens/ReportCreation.js
+++ src/screens/ReportCreation.js
@@ -3,6 +3,8 @@ import { Checkbox } from '../components/Checkbox';
 import { PrimaryButton } from '../components/PrimaryButton';
 import { NavLink } from 'react-router-dom'; 
 import React, { useState, useEffect, useContext } from 'react';
+import RoleContext from '../Context';
+
 
 
 const plantEngineerReportFields = [
@@ -50,6 +52,7 @@ const bsoReportFields = [
 export const ReportCreation = () => {
     const [fields, setFields] = useState(plantEngineerReportFields);
     const { role, setRole } = useContext(RoleContext);
+
     useEffect(() => {
         function dynamicComponentLoad() {
             if(role === 'business-services-organization') {
@@ -62,74 +65,54 @@ export const ReportCreation = () => {
     return (
         <div className='report-creation-container'>
             <div className='report-creation-header'>
-                <h4 className='report-creation-header-title'>Report Fields</h4>
+                <div className='report-creation-date'>
+                    <span>Choose the desired period</span>
+                    <input className='report-creation-input-field' id="from" name="from" placeholder='From' />
+                    <input className='report-creation-input-field' id="to" name="to" placeholder='To' />
+                </div>
                 <NavLink to='/reports'>
                     <PrimaryButton bgColor='#EB3131' color='#fff' text='Create Report' />
                 </NavLink>
             </div>
 
             <div className='report-creation-actions'>
-                <div className='report-creation-date'>
-                    <span>Choose the desired period</span>
-                    <input className='report-creation-input-field' id="from" name="from" placeholder='From' />
-                    <input className='report-creation-input-field' id="to" name="to" placeholder='To' />
+                <div className='report-creation-actions-half' style={{width: role !== 'business-services-organization' ? '50%' : '100%'}}>
+                    <h4 className='report-creation-header-title'>Report Fields</h4>
+                    <span className='report-creation-select'>Select All</span>
                 </div>
-                <span className='report-creation-select'>Select All</span>
+
+                {
+                    role !== 'business-services-organization' &&
+                    <div className='report-creation-actions-half'>
+                        <h4 className='report-creation-header-title'>Calculations</h4>
+                        <span className='report-creation-select'>Select All</span>
+                    </div>
+                }
             </div>
 
             <div className='report-creation-content'>
                 <div className='report-creation-half'>
-                    <div className='report-creation-check-item'>
-                        <Checkbox />
-                        <span className='report-creation-check-text'>Accumulated Depreciation - Machinery</span>
-                    </div>
 
-                    <div className='report-creation-check-item'>
-                        <Checkbox />
-                        <span className='report-creation-check-text'>Capital Work in Progress - Machinery</span>
-                    </div>
-
-                    <div className='report-creation-check-item'>
-                        <Checkbox />
-                        <span className='report-creation-check-text'>Depreciation Expenses</span>
-                    </div>
-
-                    <div className='report-creation-check-item'>
-                        <Checkbox />
-                        <span className='report-creation-check-text'>Gain on Disposal of Assets</span>
-                    </div>
-
-                    <div className='report-creation-check-item'>
-                        <Checkbox />
-                        <span className='report-creation-check-text'>Impairment Loss - Machinery</span>
-                    </div>
+                    {
+                        fields.slice(0, fields.length / 2).map((label, index) => (
+                            <div key={index} className='report-creation-check-item'>
+                                <Checkbox />
+                                <span className='report-creation-check-text'>{label}</span>
+                            </div>
+                        ))
+                    }
+                    
                 </div>
 
                 <div className='report-creation-half'>
-                    <div className='report-creation-check-item'>
-                        <Checkbox />
-                        <span className='report-creation-check-text'>Machinery Spare Parts Inventory</span>
-                    </div>
-
-                    <div className='report-creation-check-item'>
-                        <Checkbox />
-                        <span className='report-creation-check-text'>Maintenance & Repairs Expenses</span>
-                    </div>
-
-                    <div className='report-creation-check-item'>
-                        <Checkbox />
-                        <span className='report-creation-check-text'>Revaluation Surplus</span>
-                    </div>
-
-                    <div className='report-creation-check-item'>
-                        <Checkbox />
-                        <span className='report-creation-check-text'>Lease Liability</span>
-                    </div>
-
-                    <div className='report-creation-check-item'>
-                        <Checkbox />
-                        <span className='report-creation-check-text'>Asset Retirement Obligation</span>
-                    </div>
+                    {
+                        fields.slice(fields.length / 2, fields.length).map((label, index) => (
+                            <div key={index} className='report-creation-check-item'>
+                                <Checkbox />
+                                <span className='report-creation-check-text'>{label}</span>
+                            </div>
+                        ))
+                    }
                 </div>
             </div>
         </div>
