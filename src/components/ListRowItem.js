05bcf24bb5dbda5023e76b89048b7f46b17409ab
Cristian Scobioala - Nicoglu <t8220137@aueb.gr>
2024-05-22 00:02:27 +0300

    Implemented selection of all draft checkboxes based on the header checkbox

--- src/components/ListRowItem.js
+++ src/components/ListRowItem.js
@@ -2,12 +2,12 @@ import '../assets/style/styles.css';
 import { Checkbox } from './Checkbox';
 import React, { useState } from 'react';
 
-export const ListRowItem = ({title, reason, time}) => {
+export const ListRowItem = ({title, reason, time, firstChecked}) => {
     const [isChecked, setIsChecked] = useState(false);
 
     return (
         <div className='list-row-item'>
-            <Checkbox value={isChecked} onChange={setIsChecked} />
+            <Checkbox value={firstChecked} onChange={setIsChecked} />
             <span className='list-row-item-draft'>Draft</span>
             <span className='list-row-item-title'>
                 {title}
--- src/screens/Drafts.js
+++ src/screens/Drafts.js
@@ -9,29 +9,30 @@ import React, { useState } from 'react';
 export const Drafts = () => {
     const [headerCheckboxChecked, setHeaderCheckboxChecked] = useState(false);
 
+
     return (
         <div className='drafts-container'>
             <div className='drafts-header'>
                 <div className='drafts-header-left'>
                     <Checkbox value={headerCheckboxChecked} onChange={setHeaderCheckboxChecked}/>
-                    <img src={refreshIcon} width='25px' height='25px' />
-                    <img src={binIcon} width='25px' height='25px' />
+                    <img src={refreshIcon} class="auc-side-bar-group-icon" width='25px' height='25px' />
+                    <img src={binIcon} class="auc-side-bar-group-icon" width='25px' height='25px' />
                 </div>
                 
                 <div className='drafts-header-right'>
                     <span className='drafts-header-text'>2 drafts shown in this page</span>
-                    <img src={arrow} width='30px' height='30px' style={{opacity: 0.65}}/>
+                    <img src={arrow} class="auc-side-bar-group-icon" width='30px' height='30px' style={{opacity: 0.65}}/>
                     <span className='drafts-header-text'>Page 1 of 1</span>
-                    <img src={arrow} width='30px' height='30px' style={{opacity: 0.65, transform: 'rotate(180deg)'}}/>
+                    <img src={arrow} class="auc-side-bar-group-icon" width='30px' height='30px' style={{opacity: 0.65, transform: 'rotate(180deg)'}}/>
                 </div>
             </div>
 
             <div className='drafts-content'>
-                <ListRowItem class="draft-box" title='Assets Under Construction Report JU 2024 (1)' reason='Saved due to inaction' time='13:45'/>
-                <ListRowItem class="draft-box" title='Assets Under Construction Report JU 2024' reason='Saved but not submitted' time='12:28'/>
-                <ListRowItem class="draft-box" title='Assets Under Construction Report MA 2024' reason='Saved due to inaction' time='10:15'/>
-                <ListRowItem class="draft-box" title='Assets Under Construction Report MA 2024' reason='Saved due to inaction' time='9:58'/>
-                <ListRowItem class="draft-box" title='Assets Under Construction Report MA 2024' reason='Saved due to inaction' time='9:58'/>
+                <ListRowItem class="draft-box" title='Assets Under Construction Report JU 2024 (1)' reason='Saved due to inaction' time='13:45' firstChecked={headerCheckboxChecked}/>
+                <ListRowItem class="draft-box" title='Assets Under Construction Report JU 2024' reason='Saved but not submitted' time='12:28' firstChecked={headerCheckboxChecked}/>
+                <ListRowItem class="draft-box" title='Assets Under Construction Report MA 2024' reason='Saved due to inaction' time='10:15' firstChecked={headerCheckboxChecked}/>
+                <ListRowItem class="draft-box" title='Assets Under Construction Report MA 2024' reason='Saved due to inaction' time='9:58' firstChecked={headerCheckboxChecked}/>
+                <ListRowItem class="draft-box" title='Assets Under Construction Report MA 2024' reason='Saved due to inaction' time='9:58' firstChecked={headerCheckboxChecked}/>
             </div>
         </div>
     );
